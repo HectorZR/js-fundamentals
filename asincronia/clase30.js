7d3c243bce77bc6d7df994b279a3393e88564aa0
Hector Zurga <hjzurga@gmail.com>
2019-07-28 10:19:18 -0400

    clase32: manejando el paralelismo

--- asincronia/clase30.js
+++ asincronia/clase30.js
@@ -1,11 +1,7 @@
 const API_URL = 'https://swapi.co/api/'
 const PEOPLE_URL = 'people/:id'
 
-const onPeopleResponse = (name) => {
-    console.log(`Hola, yo soy ${name}`)
-}
-
-function obtenerPersonaje(id) {
+function obtenerPersonaje(id, callback) {
     fetch(`${API_URL}${PEOPLE_URL.replace(':id', id)}`)
     .then(response => {
         response = response.json()
@@ -14,11 +10,31 @@ function obtenerPersonaje(id) {
     })
     .then(character => {
         // console.log('Personaje: ', character)
-        onPeopleResponse(character.name)
+        if (callback) {
+            callback(character)
+        }
     })
     .catch(error => console.error('Error: ', error))
+
+    
 }
 
-obtenerPersonaje(1)
-obtenerPersonaje(2)
-obtenerPersonaje(3)
+obtenerPersonaje(1, (character) => {
+    console.log(`Hola, yo soy ${character.name}`)
+
+    obtenerPersonaje(2, (character) => {
+        console.log(`Hola, yo soy ${character.name}`)
+
+        obtenerPersonaje(3, (character) => {
+            console.log(`Hola, yo soy ${character.name}`)
+
+            obtenerPersonaje(4, (character) => {
+                console.log(`Hola, yo soy ${character.name}`)
+
+                obtenerPersonaje(5, (character) => {
+                    console.log(`Hola, yo soy ${character.name}`)
+                })
+            })
+        })
+    })
+})
